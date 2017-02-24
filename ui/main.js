1e16965c21c9c7bd88602c7e9527407f2010c17a
aalamkashyap <aalamkashyap36@gmail.com>
2017-02-24 16:38:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ var element= document.getElementById('main-text'
 );
 
 element.innerHTML = 'New Value';
+//Move the image
+var img = document.getElementById('madi'
+);
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+    
+};
