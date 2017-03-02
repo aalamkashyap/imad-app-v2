22f63cb806e64d408550c3a7d8713eee5a105bc1
aalamkashyap <aalamkashyap36@gmail.com>
2017-03-02 14:59:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,9 +115,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names = [];
-app.get('/submit-name/:name',function (req,res){
+app.get('/submit-name/',function (req,res){ //URL: /submit-name?name=xxxxx
    //Get the name from request object
-   var name = req.params.name;
+   var name = req.query.name;
    names.push(name);
    //JSON = JavaScript Object Notation
    res.send(JSON.stringify(names));
