e7fdf8b83a7d3257a0002f801b534cb388613a67
aalamkashyap <aalamkashyap36@gmail.com>
2017-03-08 15:11:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'aalamkashyap',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: db-aalamkashyap-71058
+    password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
